c7fc1392cf0ddcba2979a75027768cca26185dd6
Shinji-Nishikita <pizattu@gmail.com>
2021-09-27 00:22:39 +0900

    added material icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import "./topbar.scss";
+import {Person,Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
@@ -7,7 +8,12 @@ export default function Topbar() {
                 <div className="left">
                     <a href="#intro" className="logo">genius.</a>
                     <div className="itemContainer">
-
+                        <Person className="icon"/>
+                        <span>012 3456 7890</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>test@abc.ccc</span>
                     </div>
                 </div>
                 <div className="right"></div>
