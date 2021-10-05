02ea39b2f726abb196fbdb34b44f4df258514b72
Shinji-Nishikita <pizattu@gmail.com>
2021-10-05 23:02:28 +0900

    added portfolio content

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,19 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
+            <h1>My Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>Matching App</li>
+                <li>Game App </li>
+                <li>Bot App</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    {/* <img src="" alt="" /> */}
+                    <h3>YomiHomies</h3>
+                </div>
+            </div>
         </div>
     )
 }
