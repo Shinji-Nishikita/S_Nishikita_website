95d3685d9c3662ddc53f4b5d56732cbfb0533158
Shinji-Nishikita <pizattu@gmail.com>
2021-10-06 10:54:51 +0900

    added useState for portfoliolist

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,14 +1,39 @@
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss"
+import { useState, useEffect } from "react";
 
 export default function Portfolio() {
+    const [selected, setSelected] = useState("featured");
+    const list = [
+        {
+            id: "featured",
+            title: "Featured"
+        },
+        {
+            id: "matching app",
+            title: "Matching App"
+        },
+        {
+            id: "game app",
+            title: "Game App"
+        },
+        {
+            id: "bot app",
+            title: "Bot App"
+        },
+    ];
+
     return (
+
         <div className="portfolio" id="portfolio">
             <h1>My Portfolio</h1>
             <ul>
-                <li className="active">Featured</li>
-                <li>Matching App</li>
-                <li>Game App </li>
-                <li>Bot App</li>
+                {list.map((item) =>
+                (<PortfolioList
+                    title={item.title}
+                    active={selected === item.id}
+                    setSelected={setSelected} />)
+                )}
             </ul>
             <div className="container">
                 <div className="item">
