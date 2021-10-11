8c3c72ddef379d1af7a3c5ba8f41d5c93956870d
Shinji-Nishikita <pizattu@gmail.com>
2021-10-11 23:57:48 +0900

    fixed images for intro & contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,12 +12,12 @@ export default function Contact() {
     return (
         <div className="contact" id="contact">
             <div className="left">
-                <img src="assets/IMG_3238.JPG" alt="" />
+                <img src="assets/IMG_3238.png" alt="" />
             </div>
             <div className="right">
                 <h2>Contact</h2>
                 <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="Name" />
+                    {/* <input type="text" placeholder="Name" /> */}
                     <input type="text" placeholder="Email" />
                     <textarea placeholder="Message"></textarea>
                     <button type="submit">Send</button>
