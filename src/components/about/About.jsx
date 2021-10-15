df5d60d480309270d0b57d81540c2396ac3934e1
Shinji-Nishikita <pizattu@gmail.com>
2021-10-15 19:46:35 +0900

    added info in about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,8 +4,14 @@ export default function About() {
     return (
         <div className="about" id="about">
             <div className="left">
+                <h1>About me</h1>
+                <br></br>
                 <h2>
-                    Hello, I am Shinji Nishikita
+                    I'm a software engineer with extensive sales experience.
+                    I started learning programming by myself while working in sales. After I left my job, joined Code Chrysalis, a 3 month full-time coding bootcamp. I learned CS, data structure and full-stack engineering through some projects with international members.
+                </h2>
+                <h2>
+                    I'm strongest in PERN stack(JavaScript/React.js/Node.js/Express.js/PostgreSQL)
                 </h2>
             </div>
             <div className="right">
