9cd337571a3683390eb67ac4d5264c2a585065d2
Shinji-Nishikita <pizattu@gmail.com>
2021-10-15 23:59:15 +0900

    fixed introduction

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,9 +9,10 @@ export default function About() {
                 <h2>
                     I'm a software engineer with extensive sales experience.
                     I started learning programming by myself while working in sales. After I left my job, joined Code Chrysalis, a 3 month full-time coding bootcamp. I learned CS, data structure and full-stack engineering through some projects with international members.
+                    <br></br>
+                    I'm strongest in PERN stack(JavaScript/React.js/Node.js/Express.js/PostgreSQL)
                 </h2>
                 <h2>
-                    I'm strongest in PERN stack(JavaScript/React.js/Node.js/Express.js/PostgreSQL)
                 </h2>
             </div>
             <div className="right">
