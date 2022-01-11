68624d797fd2aed8c51230fee18e4f9ce803e093
Shinji-Nishikita <pizattu@gmail.com>
2022-01-12 00:59:21 +0900

    installed react-router-dom

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, } from "react-router-dom";
 import "./portfolio.scss";
 import { mobilePortfolio } from "../../data.js";
 
@@ -14,22 +15,22 @@ export default function Portfolio() {
 
   return (
     <div className="portfolio" id="portfolio">
-      <h1>My Portfolio</h1>
-      <div className="container">
-        {data.map(data => (
-          <div className="item"
-            onClick={() => {
-              window.location.href= data.url
-            }}
-          >
-            <img
-              src={data.img}
-              alt=""
-            />
-            <h3>{data.title}</h3>
-          </div>
-        ))}
-      </div>
+      <Router>
+        <h1>My Portfolio</h1>
+        <div className="container">
+          {data.map(data => (
+            <a className="item" href={data.url}>
+              <Switch>
+                <img
+                  src={data.img}
+                  alt=""
+                  />
+              </Switch>
+              <h3>{data.title}</h3>
+            </a>
+          ))}
+        </div>
+      </Router>
     </div>
   );
 }
